b3054e4fe99a60a7ae11078f9f6a4083cbe42dcd
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-05-30 16:54:04 +0530

    updated

--- data-engineer-associate/1.2 Databricks Lakehouse Platform/notes.sql
+++ data-engineer-associate/1.2 Databricks Lakehouse Platform/notes.sql
@@ -15,3 +15,16 @@
 1. Views(Stored) : Persisted in DB
 2. Temporary view : Session SCOPED
 3. Global Temporary Views : Cluster SCOPE
+
+
+####
+1. Databricks Lakehouse Platform 
+2. ELT with spark SQL and Python
+3. Incremental Data Processing 
+4. Production Pipelines
+5. Data Governance
+
+######
+1. 
+2. What is Databricks? : DONE 
+3. Get started with comminity Edition 
